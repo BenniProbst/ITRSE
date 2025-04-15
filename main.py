50c423c10c62bb27fc925c5d3ce4add78defd3ee
Benjamin-Elias Probst <benjamin-elias.probst@mailbox.tu-dresden.de>
2025-04-15 23:21:32 +0200

    clean data

--- main.py
+++ main.py
@@ -4,8 +4,37 @@
 # Install dependencies as needed:
 # pip install kagglehub[pandas-datasets]
 import kagglehub
+import pandas as pd
+import os
+import glob
 
 # Download latest version
 path = kagglehub.dataset_download("imtkaggleteam/fast-food-restaurants-across-america")
 
 print("Path to dataset files:", path)
+
+# Alle CSV-Dateien im Verzeichnis finden
+csv_files = glob.glob(os.path.join(path, "*.csv"))
+
+# Alle Dateien einlesen und in einer Liste speichern
+# 1. CSV-Dateien einlesen
+df1 = pd.read_csv(path+"\\Datafiniti_Fast_Food_Restaurants.csv")
+df2 = pd.read_csv(path+"\\Datafiniti_Fast_Food_Restaurants_Jun19.csv")
+
+# In ein gemeinsames DataFrame zusammenführen
+combined_df = pd.concat([df1, df2], ignore_index=True)
+combined_unique = combined_df.drop_duplicates()
+
+# Ausgabe der Spaltennamen (zur Kontrolle)
+print("Vor dem Entfernen:", len(combined_df))
+print("Nach dem Entfernen:", len(combined_unique))
+
+# 4. Optional: Ergebnis speichern
+combined_unique.to_csv(path+"\\fast_food_vereint_ohne_duplikate.csv", index=False)
+
+# Beispiel: Nur Restaurants in Kalifornien (province == "CA")
+california_restaurants = combined_df[combined_df["province"] == "CA"]
+
+# Ausgabe von ein paar Beispielen
+print(california_restaurants.head())
+
